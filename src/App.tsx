53b56de254cd1857e137af16bdbd185bb74205f8
Yosi <Yosi@YosiWebiks.com>
2024-11-04 10:39:47 +0200

    apply the context

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Route, Routes } from "react-router-dom";
 import AuthRouter from "./components/auth/AuthRouter";
 import PagesRouter from "./components/pages/PagesRouter";
+import BankContext from "./context/bank";
+import Bank from "./types/Bank";
+import Account from "./types/Account";
+import AccountContext from "./context/account";
 
 export default function App() {
+  const [bank, setBank] = useState<Bank>({
+    treasure: 10000,
+    users: [
+      {
+        name: "Yo",
+        account_id: "12345",
+      },
+      {
+        name: "Jo",
+        account_id: "54321",
+      },
+    ],
+  });
+  const [account, setAccount] = useState<Account>({
+    balance: 5000,
+    id: "12345",
+    loans: [],
+    transactions: [],
+  });
   return (
     <div className="app">
-      <Routes>
-        <Route path="auth/*" element={<AuthRouter/>} />
-        <Route path="pages/*" element={<PagesRouter />} />
-      </Routes>
+      <BankContext.Provider value={bank}>
+        <AccountContext.Provider value={account}>
+          <Routes>
+            <Route path="auth/*" element={<AuthRouter />} />
+            <Route path="pages/*" element={<PagesRouter />} />
+          </Routes>
+        </AccountContext.Provider>
+      </BankContext.Provider>
     </div>
   );
 }
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import AccountContext from '../../context/account'
+import BankContext from '../../context/bank'
+import Bank from '../../types/Bank'
+import Account from '../../types/Account'
 
 export default function Home() {
+    const account:Account = useContext(AccountContext)
+    const bank:Bank = useContext(BankContext)
+
+    const clacPrec = (b:number, t:number) =>{
+        return ((b/t)*100).toFixed(1)
+    }
   return (
-    <div>Home</div>
+    <div>
+        <h1>Balance: {account.balance}$</h1>
+        <h3>You own {clacPrec(account.balance, bank.treasure)}% of the banks treasure!!</h3>
+    </div>
   )
 }
